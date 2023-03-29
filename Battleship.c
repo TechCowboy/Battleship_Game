b24f431d726de4609de0c66f2ac55a0fa16dee7b
Norman Davie <ecompservices@gmail.com>
2023-03-29 10:25:35 -0300

    Remove non-ascii characters and remove file references

--- Battleship.c
+++ Battleship.c
@@ -15,85 +15,53 @@
 #define MAX_ROWS 10
 #define MAX_COLS 10
 
+#include "formation.h"
+
 //Functions
 void printMap(char map[][MAX_COLS]);
 int executeShot(int g,int A[][10]);
 int incomingShot(char map[][MAX_COLS]);
 void welcomeScreen (void);
 
+char map[MAX_ROWS][MAX_COLS];
+
 int main()
 {
+	char apantisi;
+	int winCounter=0,loseCounter=0,shotResult=0,myColnum=1,myrow=1,formation,row, col,A[10][10],j;
+	int g=0,i;
+
 	welcomeScreen();
-	system("chcp 1253 >nul ");	//Enable Greek Characters Command
-	FILE *fileptr=NULL;
-
-	char myColchar,apantisi,map[MAX_ROWS][MAX_COLS];
-
-	int winCounter=0,loseCounter=0,shotResult=0,myColnum=1,myrow=1,formation,row, col,A[10][10],i,j;
-	int g=0;
-	char fileRow[MAX_COLS + 2];	// Consider newline and NULL
 
 	printf("Choose the initial formation of your ships from .txt files(1/2/3/4/5/6): \n");
 	scanf("%d",&formation);
-	while (formation!=1 && formation!=2 && formation!=3 && formation!=4 && formation!=5 && formation!=6)
-	{
-		printf("Wrong formation number. Please choose the initial formation of your ships again(1/2/3/4/5/6): \n");
-		scanf("%d",&formation);
-	}
-	if (formation==1){
-		fileptr = fopen("1.txt", "r");
-	}
-	else if (formation==2){
-		fileptr=fopen("2.txt","r");
-	}
-	else if (formation==3){
-		fileptr=fopen("3.txt","r");
-	}
-	else if (formation==4){
-		fileptr=fopen("4.txt","r");
-	}
-	else if (formation==5){
-		fileptr=fopen("5.txt","r");
-	}
-	else if (formation==6){
-		fileptr=fopen("6.txt","r");
-	}
 
-	if (fileptr == NULL)
+	//Print Grid Map with ship formation
+	printf(" A B C D E F G H I J\n");
+	printf("--------------------- \n");
+	for (row = 0; row < MAX_ROWS; row++)
 	{
-		perror("File Open Error");
-		system("pause");
-		return -1;
-	}
-	else if (fileptr!=NULL)
-	{
-		//Print Grid Map with ship formation
-		printf(" A B Γ Δ Ε Ζ Η Θ Ι Κ\n");
-		printf("--------------------- \n");
-		for (row = 0; row < MAX_ROWS; row++)
+		//fgets reads characters from input and stores them into a string.
+		//A newline character stops input reading, but it is considered as a valid character
+		// Therefore, It is included in the string and copied in str.
+		printf("|");
+		for (col = 0; col < MAX_COLS; col++)
 		{
-			//fgets reads characters from input and stores them into a string.
-			//A newline character stops input reading, but it is considered as a valid character
-			// Therefore, It is included in the string and copied in str.
-			fgets(fileRow, sizeof(fileRow), fileptr);
-			printf("|");
-			for (col = 0; col < MAX_COLS; col++)
+			//Put each ship symbol from txt file into the 2D Grid Map
+			map[row][col] = pFormation[formation][row*MAX_ROWS+col];
+			if (map[row][col]=='.')
 			{
-				//Put each ship symbol from txt file into the 2D Grid Map
-				map[row][col] = fileRow[col];
-				if (map[row][col]=='.')
-				{
-					map[row][col]=' ';
-				}
-				printf("%c",map[row][col]);
-				printf("|");
+				map[row][col]=' ';
 			}
-			printf("%d",row+1);
-
-			printf("\n");
-			printf("--------------------- \n");
+			printf("%c",map[row][col]);
+			printf("|");
 		}
+		printf("%d",row+1);
+
+		printf("\n");
+		printf("--------------------- \n");
 	}
+
 	//Initialize 2D Grid A[10][10] which shows us if a ship has been hit or not using 0 for missed shot and 1 for a successful shot
 	for (i=0;i<10;i++)
 	{
@@ -107,7 +75,7 @@ int main()
 	printf("Is it your turn? (N/O): \n");
 	getchar();
 	scanf("%c", &apantisi);
-	while (apantisi!='N' && apantisi!='Ν' && apantisi!='O' && apantisi!='ν' && apantisi!='o' && apantisi!='n' && apantisi!='Ο' && apantisi!='ο')
+	while (apantisi!='N' && apantisi!='n' && apantisi!='O' && apantisi!='o')
 	{
 		printf("\nIncorrect Order!Enter <<N>> in case it is your turn to shoot or <<O>> if it is your opponent's turn: \n");
 		printf("Is it your turn? (N/O): \n");
@@ -116,7 +84,7 @@ int main()
 	}
 
 	//In case it is user's turn
-	if (apantisi=='Ν' || apantisi=='n' || apantisi=='N' || apantisi=='ν')
+	if (apantisi=='N' || apantisi=='n')
 	{
 		shotResult=executeShot(g,A);
 		if (shotResult==1)
@@ -153,7 +121,7 @@ int main()
 
 	}
 	//In case user is taking fire
-	else if (apantisi=='O' || apantisi=='o' || apantisi=='ο' || apantisi=='Ο' )
+	else if (apantisi=='O' || apantisi=='o')
 	{
 		loseCounter=incomingShot(map);
 		printMap(map);
@@ -180,9 +148,6 @@ int main()
 			}
 		}
 	}
-	fclose(fileptr);
-
-	system("pause");
 
 	return 0;
 }
@@ -224,7 +189,7 @@ void welcomeScreen (void) {
 void printMap(char map[][MAX_COLS])
 {
 	int row,col;
-	printf(" A B Γ Δ Ε Ζ Η Θ Ι Κ\n");
+	printf(" A B C D E F G H I J\n");
 	printf("--------------------- \n");
 	for (row = 0; row < MAX_ROWS; row++)
 	{
@@ -248,8 +213,8 @@ void printMap(char map[][MAX_COLS])
 int executeShot(int g,int A[][10])
 {
 	static int shotResult=0,i,pin[100],pin2[100];
-	int l,c;
-	static char apotelesma='Α';
+	int c;
+	static char apotelesma='A';
 
 	srand ( time(NULL) );
 	l = rand() % 10;
@@ -276,7 +241,7 @@ int executeShot(int g,int A[][10])
 	pin[g]=c;                     //Place in the grid
 	pin2[g]=l;
 
-	if (apotelesma=='ε' || apotelesma=='Ε' || apotelesma=='e' || apotelesma=='E')
+	if (apotelesma=='e' || apotelesma=='E')
 	{
 		//If shot fired is successful, then our program chooses to shot next fire right
 		if (  pin[g-1]+1!=-53)
@@ -294,14 +259,14 @@ int executeShot(int g,int A[][10])
 	//User enters result of whether shot was successful or not
 	scanf("%s",&apotelesma);
 
-	while (apotelesma!='A' && apotelesma!='E' && apotelesma!='ε' && apotelesma!='α' && apotelesma!='Ε' && apotelesma!='Α' && apotelesma!='e' && apotelesma!='a' )
+	while (apotelesma!='A' && apotelesma!='E'&& apotelesma!='e' && apotelesma!='a' )
 	{
 		printf("Wrong Input!\nResult of the shot you fired:(Enter <<E>> for success or <<A>> for missed shot)\n");
 		printf("Shot %c %d: \n",c,l);
 		scanf("%s",&apotelesma);
 
 	}
-	if (apotelesma=='E' || apotelesma=='e' || apotelesma=='ε' || apotelesma=='Ε' )
+	if (apotelesma=='E' || apotelesma=='e')
 	{
 		//If shot was successful, function returns 1 and counter is increased
 		return 1;
@@ -406,7 +371,7 @@ int incomingShot(char map[][MAX_COLS])
 		printf("Result of incoming shot: Missed!\n");
 	}
 	//If ship has been hit replace ship symbol with a '*' in array
-	else if (map[row][col]=='Π' || map[row][col]=='Ν' || map[row][col]=='Κ' || map[row][col]=='Φ' || map[row][col]=='Υ'){
+	else if (map[row][col]!=' '){
 		map[row][col]='*';
 	}
 
